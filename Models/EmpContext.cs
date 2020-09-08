9ae63b719afedb9dab5aaae27b90fb9695df8b4e
sgopathi2 <saipranath.gopathi@gmail.com>
2020-09-08 12:09:09 +0530

    After Adding Emp&Context Class and changes in WebConfig.

---
+++ Models/EmpContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MVC.Models
+{
+    public class EmpContext : DbContext
+    {
+        public EmpContext() : base("EmpContext")
+        {
+
+        }
+        public DbSet<Emp> Emps { get; set; }
+    }
+}
